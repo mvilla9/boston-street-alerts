c98490878ada192f4667deb60a4414b069cffd22
mvilla9 <mateo.john.villa@gmail.com>
2024-03-15 21:57:01 -0400

    Initial Commit

---
+++ boston-street-alerts/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  boston-street-alerts
+//
+//  Created by Mateo Villa on 3/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ boston-street-alerts/boston_street_alertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  boston_street_alertsApp.swift
+//  boston-street-alerts
+//
+//  Created by Mateo Villa on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct boston_street_alertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
