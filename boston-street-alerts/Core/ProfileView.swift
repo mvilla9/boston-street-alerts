c1f67b63a4ba78c35bf4aacaedefdd95e7128a18
mvilla9 <mateo.john.villa@gmail.com>
2024-03-15 22:39:53 -0400

    Add simple profile page

---
+++ boston-street-alerts/Core/ProfileView.swift
@@ -0,0 +1,42 @@
+//
+//  ProfileView.swift
+//  boston-street-alerts
+//
+//  Created by Mateo Villa on 3/15/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var username: String
+    var body: some View {
+        List {
+            HStack {
+                Text("AA")
+                    .frame(width: 50, height: 50)
+                    .background(Color.gray)
+                    .clipShape(Circle())
+                VStack (alignment: .leading){
+                    Text(username)
+                    Text("Member since 2024")
+                        .font(.subheadline)
+                        .italic()
+                }
+            }
+            HStack {
+                Image(systemName: "gear")
+                Text("Settings")
+            }
+            HStack {
+                Image(systemName: "arrow.uturn.left")
+                Text("Log Out")
+            }
+        }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView(username: "John Doe")
+    }
+}
